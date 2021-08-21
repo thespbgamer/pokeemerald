63799ad70f7eb2bc5a49d4d7fea2c6136f5141b4
thespbgamer <30112696+thespbgamer@users.noreply.github.com>
2021-08-21 15:04:51 +0100

    test

--- src/start_menu.c
+++ src/start_menu.c
@@ -309,7 +309,7 @@ static void BuildNormalStartMenu(void)
     AddStartMenuAction(MENU_ACTION_PLAYER);
     AddStartMenuAction(MENU_ACTION_SAVE);
     AddStartMenuAction(MENU_ACTION_OPTION);
-    AddStartMenuAction(MENU_ACTION_EXIT);
+    //AddStartMenuAction(MENU_ACTION_EXIT);
 }
 
 static void BuildSafariZoneStartMenu(void)
