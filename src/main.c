37bbf2b0dc6e31dd0a84f2eeae893e20954ed7c5
thespbgamer <thespbgamer@outlook.com>
2023-06-17 22:19:53 +0100

    Faster HP Drain, Surf Dismount Ground Effects, Improving the WaitForVBlank function

--- src/main.c
+++ src/main.c
@@ -409,8 +409,9 @@ static void WaitForVBlank(void)
 {
     gMain.intrCheck &= ~INTR_FLAG_VBLANK;
 
-    while (!(gMain.intrCheck & INTR_FLAG_VBLANK))
-        ;
+    // while (!(gMain.intrCheck & INTR_FLAG_VBLANK))
+    //     ;
+    VBlankIntrWait();
 }
 
 void SetTrainerHillVBlankCounter(u32 *counter)
