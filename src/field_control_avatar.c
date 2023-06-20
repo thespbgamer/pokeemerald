8cefae0c15d49f50f397ecd4f3d9ddc962dd7d1a
thespbgamer <thespbgamer@outlook.com>
2023-06-20 12:46:18 +0100

    Fixed changing bike with dexnav

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -105,7 +105,7 @@ void FieldGetPlayerInput(struct FieldInput *input, u16 newKeys, u16 heldKeys)
                 input->pressedAButton = TRUE;
             if (newKeys & B_BUTTON)
                 input->pressedBButton = TRUE;
-            if (newKeys & R_BUTTON && !FlagGet(FLAG_SYS_DEXNAV_SEARCH))
+            if (newKeys & R_BUTTON && !FlagGet(FLAG_SYS_DEXNAV_SEARCH) && !TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_BIKE))
                 input->pressedRButton = TRUE;
         }
 
